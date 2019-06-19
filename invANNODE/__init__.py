a1570dd809fb6c30a86f9ada2a11807fa90c768e
gusmaogabriels <gusmaogabriels@gmail.com>
2019-06-19 14:49:42 -0400

    Added diagram.

---
+++ invANNODE/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 19 08:28:22 2019
+
+@author: ggusmao3
+"""
+
+# import dependencies
+
+import os
+import sys
+import inspect
+import re
+from copy import copy
+from string import Template
+import numpy as np
+
+from .ANNs import perceptron
+
+
